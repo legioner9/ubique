5fc95659ed4360fab498eb69176041ea55238c24
[alt]legioner9 <legioner9@inbox.ru>
2024-10-20 04:11:45 +0700

    <> ##

---
+++ .s/Store/.stl.d/stl.boot.d/stl.boot.fn.sh.d/boot_stl_XXX.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+boot_stl_XXX() {
+
+    local FNN=${FUNCNAME[0]}
+    local PPWD=$PWD
+    local ARGS=("$@")
+
+    local ARGS0="$1"
+    local ARGS1="$2"
+    local ARGS2="$3"
+    local ARGS3="$4"
+
+    local NARGS=$#
+
+    local item=
+
+    local sh_file=${HOME}/.stl.d/stl.boot.d/stl.boot.fn.sh.d/boot_stl_XXX.sh
+
+        if [[ "-h" == "$1" ]]; then
+        echo -e "
+MAIN: ${FNN} :: 
+TAGS: 
+\$1 
+[, \$2]
+[, \$N last arg DEBAG CNTL
+    if '_i' debag action, use: [ \$di -eq 1 ] && {debag action} ]
+
+UCNT:
+    _?
+CNTL: 
+    _e      : _edit body            : _edit file://${sh_file}
+    _d      : exec tst_dir_fn       : . file://${tst_dir_file}
+    _f1     : exec flow_dir_file_1  : . file://${flow_dir_file_1}
+    _f1_e   : _edit flow_dir_file_1 : _edit file://${flow_dir_file_1}
+    _hie_m  : more hie_file         : more file://${hie_file} 
+    _hie_e  : _edit hie_file        : _edit file://${hie_file} 
+    _data_e : _edit data_dir        : _edit file://${data_dir} 
+
+RETU: ( result>stdout, return 0 | data | change to ptr |  fs_structure | ...)
+ERNO: ( if ... return 0 | if ... return 1 )
+EXAM: 
+    ${FNN}
+"
+}
